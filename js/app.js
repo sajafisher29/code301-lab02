a966c355d5f3ab4bac48cc7f215675393f105503
sajafisher29 <sajafisher@gmail.com>
2019-07-10 09:37:38 -0700

    Starting notes in js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+'use strict'
+//////////////////////////////////////
+
+//build constructor function to build/display each photo
+//use AJAX $.get() to read the JSON file data
+// use jQuery to make a copy of the HMTL template of the photo component
+//for each object, fill in the duplicated template with its properties, then append the copy to the DOM
+
