6a834646341e61c6eae424e37a3325f9369b5aeb
sajafisher29 <sajafisher@gmail.com>
2019-07-10 15:23:26 -0700

    Verifying end of lab equalizing

--- js/app.js
+++ js/app.js
@@ -5,4 +5,3 @@
 //use AJAX $.get() to read the JSON file data
 // use jQuery to make a copy of the HMTL template of the photo component
 //for each object, fill in the duplicated template with its properties, then append the copy to the DOM
-
